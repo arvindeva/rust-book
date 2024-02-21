26b7c9e9f7df2624e3cc7871843f4c3c0d9cac37
arvindeva <arvindeva@gmail.com>
2024-02-21 08:45:21 +0100

    rust book

---
+++ functions/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
